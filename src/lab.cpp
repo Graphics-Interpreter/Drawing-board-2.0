bf521afaadf6072ca8b02999452abc01bcc3658a
lw <cui@hellolw.com>
2016-06-04 16:18:30 +0800

    finish drawing polygon

--- src/lab.cpp
+++ src/lab.cpp
@@ -8,8 +8,9 @@ int main()
 {
     // create the window
     sf::RenderWindow window(sf::VideoMode(800, 600), "Computer Graphics");
-
-    sf::Vector2i pressPostion{-1, -1}, localPosition;
+    std::vector<sf::Vector2i> points;
+    sf::Vector2i pressPostion, localPosition;
+    bool valid = false;
     // run the program as long as the window is open
     while (window.isOpen()) {
         // check all the window's events that were triggered since the last iteration of the loop
@@ -18,26 +19,27 @@ int main()
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
-            if (event.type == sf::Event::MouseButtonPressed && pressPostion.x == -1)
+            if (event.type == sf::Event::MouseButtonPressed)
                 if (event.mouseButton.button == sf::Mouse::Left) {
+                    if (!valid) points.clear(); valid = true; 
                     pressPostion.x = event.mouseButton.x;
                     pressPostion.y = event.mouseButton.y;
-                }
-            if (event.type == sf::Event::MouseButtonReleased)
-                if (event.mouseButton.button == sf::Mouse::Left) {
-                    pressPostion.x = -1;
-                    pressPostion.y = -1;
+                    points.push_back(pressPostion);
+                } else if (event.mouseButton.button == sf::Mouse::Right) {
+                    valid = false; points.push_back(points.front());
+                    // TODO
                 }
         }
 
         // clear the window with black color
         window.clear(sf::Color::White);
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+        if (valid) {
             localPosition = sf::Mouse::getPosition(window); 
-
-            if (pressPostion.x != -1)
-                window.draw(Line(pressPostion, localPosition));
+            if (!points.empty())
+                window.draw(Line(points.back(), localPosition));
         }
+        for (int i = 0; i + 1 < points.size(); i++)
+            window.draw(Line(points[i], points[i + 1]));
         // end the current frame
         window.display();
     }
